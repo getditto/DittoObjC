1689ff76f1914d6ec527e3b5b8d4f78dc9711677
Ben Chatelain <ben@ditto.live>
2022-01-28 13:03:26 -0700

    🔖 Version 1.1.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DittoObjC",
-            url: "https://github.com/getditto/DittoObjC/releases/download/1.0.19/DittoObjC.xcframework.zip",
-            checksum: "08526a7e68415e2499674d171fd929e6c8cf3571581a8e7d7619d735e7612770"
+            url: "https://github.com/getditto/DittoObjC/releases/download/1.1.0/DittoObjC.xcframework.zip",
+            checksum: "f4e1ea50cc21986f7cb23c2be50519d61cea43b99d50ddff794b4a164dd4cd27"
         )
     ]
 )
